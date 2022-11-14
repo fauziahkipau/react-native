4b6497dd484a76617f8eb3906a361526ad7166b7
kipau <riskyfauziah5@gmail.com>
2022-11-14 20:59:24 +0700

    Splitting Components Into Smaller Components

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
 import { useState } from "react";
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  ScrollView,
-  FlatList,
-} from "react-native";
+import { Button, StyleSheet, TextInput, View, FlatList } from "react-native";
+
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
   const [enteredGoalText, sentEnteredGoalText] = useState("");
@@ -38,11 +32,7 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItems}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem text={itemData.item.text}></GoalItem>;
           }}
           keyExtractor={(item, index) => {
             return item.id;
@@ -77,14 +67,5 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 4,
-  },
-  goalItems: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-  },
-  goalText: {
-    color: "white",
   },
 });
